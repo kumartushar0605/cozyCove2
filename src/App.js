c230f840a2ad428c7b5d11c31737d0b84e95a752
Tushar Kumar <t75332007@gmail.com>
2024-09-22 01:48:01 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Dogs from "./Components/Dogs";
+import DogsDecp from "./Components/DogsDecp";
+import Form from './Components/Form'
+import Cats from "./Components/Cats";
+import About from "./Components/About";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/dogs" element={<Dogs/>}/>
+        <Route path="/dogsDesc" element={<DogsDecp/>}/>
+        <Route path="/form" element={<Form/>}/>
+        <Route path="/cats" element={<Cats/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ColorModeScript,ChakraProvider } from '@chakra-ui/react';
+import ColorModeSwitcher from './ColorModeSwitcher';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ColorModeScript/>
+    <ChakraProvider>
+
+    <ColorModeSwitcher/>
+    {/* colourModeSwitcher is use the change the theme of the page to dark or light */}
+
     <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
